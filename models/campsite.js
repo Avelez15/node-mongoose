6c24db56fa6f90d2def0e34eec50407141edd0fc
Alejandro Velez <velez.alejandro4@gmail.com>
2023-06-07 22:42:29 -0400

    mongoose part 3

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 //below is used to create a schema, creating configurations for the doc.
 const campsiteSchema = new Schema({
     name: {
@@ -11,10 +30,11 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    //below causes every campsite document to have multiple comment documents stored within an array.
+    comments: [commentSchema]
 }, {
-    timestamps: true,
-
+    timestamps: true
 });
 //second optional argument is used to set configuration options
 //below creates a model, const must be capitalized and name string must be the same, second argument must be schema you wish to use.
